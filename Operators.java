c5762b149562698c3ca62122f1fd714e50272c9b
Divyajain15 <divyaofficial1507@gmail.com>
2026-08-16 14:17:20 +0530

    code1

---
+++ Operators.java
@@ -0,0 +1,16 @@
+class Operators{
+    public static void main(String[] args)
+    {
+        int num1 = 10 , num2 = 5 ;
+        System.out.println("Two numbers are: " + num1 + "," + num2 );
+        System.out.println("Addition:"+ (num1+num2));
+        System.out.println("Subtraction:"+ (num1-num2));
+        System.out.println("Multiplication:"+ (num1*num2));
+        System.out.println("Division:"+ (num1/num2));
+        System.out.println("num1 > num2:"+ (num1>num2));
+        System.out.println("num1 < num2:"+ (num1<num2));
+        System.out.println("num1 == num2:"+ (num1==num2));
+        System.out.println("(num1>num2)&&(num1!=num2):"+ ((num1>num2)&&(num1!=num2)));
+
+    }
+}
